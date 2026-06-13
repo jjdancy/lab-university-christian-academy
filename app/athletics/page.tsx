cf92fbae0257a822b4237421774d4c6714501a81
jjdancy <jjdancy@waveride.co>
2026-06-12 20:08:57 -0400

    Athletics: nudge coach photo to top

--- app/athletics/page.tsx
+++ app/athletics/page.tsx
@@ -547,7 +547,7 @@ export default async function AthleticsPage() {
                           <img
                             src={coach.photoUrl}
                             alt={coach.photoAlt}
-                            className="h-full w-full object-cover object-[center_5%] scale-110"
+                            className="h-full w-full object-cover object-top scale-110"
                           />
                         </div>
                       </div>
